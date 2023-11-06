ebbe4befebe3959dd4f879f3bc4cce8d9b865798
Krishna <cu.17bcs1796@gmail.com>
2023-11-06 09:47:37 +0530

    :rocket: project completed.

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 const http = require("http");
 const fs = require("fs");
-var requests = require("requests");
+const https = require("https");
 
 const homeFile = fs.readFileSync("Home.html", "utf-8");
 
-const server = http.createServer((res, req)=>{
-    if(req.url == "/"){
-        requests("https://api.openweathermap.org/data/2.5/weather?q=pune&appid=76f1561a36750427f59dfe127ce79fef")
-.on('data', function (chunk) {
-  console.log(chunk)
-})
-.on('end', function (err) {
-  if (err) return console.log('connection closed due to errors', err);
- 
-  console.log('end');
-});
-    }
+const replaceVal = (tempVal, orgVal) =>{
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+  console.log(orgVal.weather[0].main);
+  return temperature;
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    const apiURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=pune&appid=76f1561a36750427f59dfe127ce79fef";
+
+    https.get(apiURL, (apiRes) => {
+      let data = "";
+
+      apiRes.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      apiRes.on("end", () => {
+        try {
+          const weatherData = JSON.parse(data);
+          const arrayData = [weatherData];
+          // console.log(arrayData);
+
+          const realTimeData = arrayData.map((val)=>replaceVal(homeFile, val)).join("");
+
+          // You can process the weatherData here as needed
+          // Now, you can send a response to the client
+          // res.write(realTimeData);
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(realTimeData);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+        }
+      });
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page not found");
+  }
 });
 
-server.listen(8000, '127.0.0.1');
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
